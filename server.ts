43236822da699c04f308daf49ceb8edb0858a531
NikitaVirachev <guardsolder@gmail.com>
2025-11-13 23:36:20 +0300

    feat #1: handle uncaught exception

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
 import app from './app';
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 3000;
