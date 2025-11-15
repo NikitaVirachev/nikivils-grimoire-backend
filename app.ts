2b1318b166d7533523277d63613e0942a6886b5d
NikitaVirachev <guardsolder@gmail.com>
2025-11-15 19:48:38 +0300

    feat #1: reply static files for main route

--- app.ts
+++ app.ts
@@ -1,5 +1,19 @@
+import path from 'path';
 import express from 'express';
 
 const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Body parser
+app.use(express.json());
+
+// 2) Static files
+const frontendDir: string = process.env.FRONTEND_DIR || path.join(__dirname, 'public');
+app.use(express.static(frontendDir));
+
+// Main route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendDir, 'index.html'));
+});
 
 export default app;
