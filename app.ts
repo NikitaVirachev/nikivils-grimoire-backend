1dd8bc17bc8d9f5c342bbe85bf89fa700c9eb5d2
NikitaVirachev <guardsolder@gmail.com>
2025-11-16 17:40:34 +0300

    feat #1: use morgan if mode is development

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import morgan from 'morgan';
 import globalErrorHandler from './controllers/errorController';
 
 const app = express();
@@ -11,6 +12,10 @@ app.use(express.json());
 // 2) Static files
 const frontendDir: string = process.env.FRONTEND_DIR || path.join(__dirname, 'public');
 app.use(express.static(frontendDir));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Main route
 app.get('/', (req, res) => {
