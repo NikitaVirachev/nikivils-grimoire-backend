461dd7bce8a6f0c08ba53d5009efcec8063e82db
virachev.n <virachev.n@energosoft.net>
2026-07-17 18:50:44 +0300

    feat #4: connect to mongo db

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import './loadEnv';
 import app from './app';
 
@@ -10,7 +11,13 @@ process.on('uncaughtException', (err) => {
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+  console.log(`PID: ${process.pid}`);
 });
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/nikivils-grimoire')
+  .then(() => console.log('DB connection successful!'))
+  .catch((e) => console.error(e));
 
 process.on('unhandledRejection', (err: Error) => {
   console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
