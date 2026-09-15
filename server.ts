373cb05687f43d3b737b0ee08b663bd68d7c925d
NikitaVirachev <guardsolder@gmail.com>
2026-09-15 18:22:12 +0300

    feat #7: start listening app only after connecting to db

--- server.ts
+++ server.ts
@@ -1,27 +1,50 @@
 import mongoose from 'mongoose';
+
 import './loadEnv';
+
 import app from './app';
 
-process.on('uncaughtException', (err) => {
+import { initializeServices } from './container';
+
+process.on('uncaughtException', (err: Error) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
+
   process.exit(1);
 });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/nikivils-grimoire')
-  .then(() => console.log('DB connection successful!'))
-  .catch((e) => console.error(e));
+let server: ReturnType<typeof app.listen>;
+
+async function start() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/nikivils-grimoire');
+
+  console.log('DB connection successful!');
+
+  initializeServices();
+
+  server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
+}
+
+start().catch((err: Error) => {
+  console.error('Failed to start application');
+  console.error(err);
+
+  process.exit(1);
+});
 
 process.on('unhandledRejection', (err: Error) => {
   console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
-  server.close(() => {
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
     process.exit(1);
-  });
+  }
 });
