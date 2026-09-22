b4772c89458b5bbfee4d5a041d2fb39f3c5c5413
NikitaVirachev <guardsolder@gmail.com>
2026-09-22 21:42:55 +0300

    feat #7: leave check that type is allowed only using file-type

--- middleware/mediaUploadMiddleware.ts
+++ middleware/mediaUploadMiddleware.ts
@@ -1,29 +1,11 @@
 import multer from 'multer';
 
-const ALLOWED_MIME_TYPES = new Set([
-  'image/jpeg',
-  'image/png',
-  'image/webp',
-  'image/gif',
-  'image/avif',
-]);
-
 const uploadMedia = multer({
   storage: multer.memoryStorage(),
 
   limits: {
     fileSize: Number(process.env.MAX_IMAGE_SIZE),
     files: 1,
-  },
-
-  fileFilter: (_req, file, callback) => {
-    if (!ALLOWED_MIME_TYPES.has(file.mimetype)) {
-      callback(new Error(`Unsupported image type: ${file.mimetype}`));
-
-      return;
-    }
-
-    callback(null, true);
   },
 });
 
