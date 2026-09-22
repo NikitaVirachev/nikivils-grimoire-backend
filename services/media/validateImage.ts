d916c48c716ad5cf17361f8a96745bc4ab063310
NikitaVirachev <guardsolder@gmail.com>
2026-09-22 22:19:48 +0300

    Revert "fix #7: remove unnecessary try catch"
    This reverts commit 5fe25160947ae8a93e1a757e2aeb2fd7d381e77b.

--- services/media/validateImage.ts
+++ services/media/validateImage.ts
@@ -31,17 +31,23 @@ const validateImageasync = async (buffer: Buffer): Promise<ValidatedImage> => {
     throw new AppError(`Unsupported image type: ${detectedType.mime}`, 415);
   }
 
-  const metadata = await sharp(buffer, {
-    /*
-     * Do not allow the image size to increase after unpacking.
-     */
-    limitInputPixels: Number(process.env.MAX_IMAGE_PIXELS),
+  let metadata: sharp.Metadata;
 
-    /*
-     * We retain strict warning handling for untrusted files.
-     */
-    failOn: 'warning',
-  }).metadata();
+  try {
+    metadata = await sharp(buffer, {
+      /*
+       * Do not allow the image size to increase after unpacking.
+       */
+      limitInputPixels: Number(process.env.MAX_IMAGE_PIXELS),
+
+      /*
+       * We retain strict warning handling for untrusted files.
+       */
+      failOn: 'warning',
+    }).metadata();
+  } catch {
+    throw new Error('Invalid or corrupted image');
+  }
 
   if (!metadata.width || !metadata.height) {
     throw new Error('Unable to determine image dimensions');
