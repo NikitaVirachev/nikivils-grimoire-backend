3b80c0182d024e69e35565ac8fb3cecc2a97f31f
virachev.n <virachev.n@energosoft.net>
2026-09-25 17:47:39 +0300

    feat #7: update post controller

--- controllers/postController.ts
+++ controllers/postController.ts
@@ -1,36 +1,52 @@
-import Post from '../models/postModel';
-import AppError from '../utils/appError';
+import PostService from '../services/post';
 import catchAsync from '../utils/catchAsync';
 
+const postService = new PostService();
+
 export const getAllPosts = catchAsync(async (req, res, next) => {
-  const posts = await Post.find();
-  res.status(200).json({ status: 'success', data: { posts } });
-});
-
-export const getPost = catchAsync(async (req, res, next) => {
-  const post = await Post.findById(req.params.id);
-  res.status(200).json({ status: 'success', data: { post } });
-});
-
-export const createPost = catchAsync(async (req, res, next) => {
-  const newPost = await Post.create(req.body);
+  const posts = await postService.getPublished();
 
   res.status(200).json({
     status: 'success',
+
+    results: posts.length,
+
     data: {
-      post: newPost,
+      posts,
+    },
+  });
+});
+
+export const getPost = catchAsync(async (req, res, next) => {
+  const post = await postService.getById(req.params.id);
+
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      post,
+    },
+  });
+});
+
+export const createPost = catchAsync(async (req, res, next) => {
+  const newPost = await postService.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+
+    data: {
+      newPost,
     },
   });
 });
 
 export const updatePost = catchAsync(async (req, res, next) => {
-  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  const post = await postService.update(req.params.id, req.body);
 
   res.status(200).json({
     status: 'success',
+
     data: {
       post,
     },
@@ -38,14 +54,7 @@ export const updatePost = catchAsync(async (req, res, next) => {
 });
 
 export const deletePost = catchAsync(async (req, res, next) => {
-  const post = await Post.findByIdAndDelete(req.params.id);
+  await postService.delete(req.params.id);
 
-  if (!post) {
-    return next(new AppError('No document found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: null,
-  });
+  res.status(204).send();
 });
